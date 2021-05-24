cde4bc6fb4b96bc4723c7689f07f4b9a6ff10fbb
amlsakr <amlsakr9@gmail.com>
2021-05-24 12:44:06 +0200

    Adding navigation componenet dependenct, adding nav grapg.xml, create 3 Fragments Welcome screen fragment home fragment and jobDetailsFragment

--- app/src/main/java/com/example/jobsapp/WelcomeScreenActivity.kt
+++ app/src/main/java/com/example/jobsapp/WelcomeScreenActivity.kt
@@ -3,7 +3,7 @@ package com.example.jobsapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class WelcomeScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
